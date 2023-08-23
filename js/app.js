0d86903f25abe04445ec48f69c3745976f74e5da
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-23 08:20:48 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -123,3 +123,22 @@ function sumAll(arr) {
 }
   
 console.log(sumAll([5, 10]));
+
+//destroyer
+function destroyer(arr) {
+  const valsToRemove = Object.values(arguments).slice(1);
+  const filteredArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let removeElement = false;
+    for (let j = 0; j < valsToRemove.length; j++) {
+      if (arr[i] === valsToRemove[j]) {
+        removeElement = true;
+      }
+    }
+    if (!removeElement) {
+      filteredArray.push(arr[i]);
+    }
+  }
+  return filteredArray;
+}
