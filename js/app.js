b04ea1ad147550fb14c107859cbb287c98abb475
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-25 08:26:33 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -142,3 +142,24 @@ function destroyer(arr) {
   }
   return filteredArray;
 }
+
+//sum of all prime numbers
+function sumPrimes(num) {
+  // Helper function to check primality
+  function isPrime(num) {
+    const sqrt = Math.sqrt(num);
+    for (let i = 2; i <= sqrt; i++) {
+      if (num % i === 0)
+        return false;
+    }
+    return true;
+  }
+
+  // Check all numbers for primality
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
+}
