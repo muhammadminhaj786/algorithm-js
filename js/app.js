7fddc96348f2fad3acb4261ac6229125e6488a35
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-27 08:05:39 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -182,3 +182,23 @@ function fearNotLetter(str) {
 
 // test here
 fearNotLetter("abce");
+
+//Wherefore art thou
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  const collectionMatches = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    let foundMismatch = false;
+
+    for (const sourceProp in source) {
+      if (collection[i][sourceProp] !== source[sourceProp]) {
+        foundMismatch = true;
+      }
+    }
+    if (!foundMismatch) {
+      collectionMatches.push(collection[i]);
+    }
+  }
+  return collectionMatches;
+}
