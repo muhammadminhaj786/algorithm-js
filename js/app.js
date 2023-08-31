33ba6b06e5f884af7e1a373b4cfc054d1e8d8d67
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-30 22:55:08 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -202,3 +202,22 @@ function whatIsInAName(collection, source) {
   }
   return collectionMatches;
 }
+
+Sum All Odd Fibonacci Numbers
+function sumFibs(num) {
+  let prevNumber = 0;
+  let currNumber = 1;
+  let result = 0;
+  while (currNumber <= num) {
+    if (currNumber % 2 !== 0) {
+      result += currNumber;
+    }
+    currNumber += prevNumber;
+    prevNumber = currNumber - prevNumber;
+  }
+
+  return result;
+}
+
+// test here
+sumFibs(4);
