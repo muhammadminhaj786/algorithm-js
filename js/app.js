fb4243d43fee27d1d2d1d81a26da2175188db7e1
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-09-23 22:27:49 -0700

    Fibonacci sequence

--- js/app.js
+++ js/app.js
@@ -239,3 +239,14 @@ function rot13(str) {
 rot13("SERR PBQR PNZC!");
 
 var new = 'string'
+
+
+//Fibonacci sequence
+let number = 10;
+let n1 = 0, n2 = 1, nextTerm;
+for (let i =1;i<number;i++){
+    console.log(n1)
+    nextTerm = n1 + n2
+    n1 = n2
+    n2 = nextTerm
+}
