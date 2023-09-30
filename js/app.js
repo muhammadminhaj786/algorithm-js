c6500ed6656cc01a44aea3f4a673fa9758943132
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-09-29 22:41:13 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -240,7 +240,26 @@ rot13("SERR PBQR PNZC!");
 
 var new = 'string'
 
-
+//Quick sort
+const quickSort = arr => {
+  const a = [...arr];
+  if (a.length < 2) return a;
+  const pivotIndex = Math.floor(arr.length / 2);
+  const pivot = a[pivotIndex];
+  const [lo, hi] = a.reduce(
+    (acc, val, i) => {
+      if (val < pivot || (val === pivot && i != pivotIndex)) {
+        acc[0].push(val);
+      } else if (val > pivot) {
+        acc[1].push(val);
+      }
+      return acc;
+    },
+    [[], []]
+  );
+  return [...quickSort(lo), pivot, ...quickSort(hi)];
+};
+    
 //Fibonacci sequence
 let number = 10;
 let n1 = 0, n2 = 1, nextTerm;
