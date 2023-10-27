76dbc1342690cb1d4ea40ed5c119414fca2e3279
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-10-27 21:11:04 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -280,3 +280,26 @@ var n = 1
  var n1 = 2;
 console.log(n)
 var b = n1
+
+// remove duplicate characters 
+function removeDuplicateChar(str){
+  var len = str.length,
+      char, 
+      charCount = {}, 
+      newStr = [];
+  for(var i =0; i<len; i++){
+    char = str[i];
+    if(charCount[char]){
+      charCount[char]++;
+    }
+    else
+      charCount[char] = 1;
+  }
+  for (var j in charCount){
+    if (charCount[j]==1)
+       newStr.push(j);
+  }
+  return newStr.join('');
+}
+
+removeDuplicateChar('hi my name is name and name')
